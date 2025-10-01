24e007f758c3fe69a26b760b086caa6164de5e8e
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-01 10:55:17 +0100

    add mempool service

---
+++ src/service/ServiceMempool.java
@@ -0,0 +1,45 @@
+package service;
+
+import domain.Transaction;
+import domain.enums.NiveauFrais;
+import repository.TransactionRepository;
+import util.CalculateurFrais;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ServiceMempool {
+    
+    private final TransactionRepository transactionRepository;
+    
+    public ServiceMempool() {
+        this.transactionRepository = new TransactionRepository();
+    }
+    
+    public List<Transaction> obtenirMempool() {
+        return transactionRepository.trouverTous().stream()
+            .filter(t -> t.getStatut().name().equals("EN_ATTENTE"))
+            .sorted(Comparator.comparingDouble(Transaction::getFrais).reversed())
+            .collect(Collectors.toList());
+    }
+    
+    public int calculerPositionDansMempool(double fraisTransaction) {
+        List<Transaction> mempool = obtenirMempool();
+        
+        for (int i = 0; i < mempool.size(); i++) {
+            if (fraisTransaction >= mempool.get(i).getFrais()) {
+                return i + 1;
+            }
+        }
+        return mempool.size() + 1;
+    }
+    
+    public long estimerTempsAttente(int position) {
+        return position * 10L; 
+    }
+    
+    public void genererTransactionsAleatoires(int nombre) {
+        // Implémentation pour générer des transactions aléatoires
+        // pour simuler l'activité du réseau
+    }
+}
